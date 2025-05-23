d918ebda42049856290d34ad7bfff8f1a1ed1f2a
Maël <119709626+maeldolium@users.noreply.github.com>
2025-05-23 19:50:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const getConnection = async () => {
     const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'gymtech'
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        database: process.env.DB_NAME ||'gymtech'
     });
     return connection;
 };
